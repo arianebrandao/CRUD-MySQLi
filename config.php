7164300054e94b393eb0ac94db4eb9361e3f6bbc
Ariane Brandão <ariane_mad@hotmail.com>
2018-11-03 00:39:40 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+  define('DB_HOSTNAME', 'localhost');
+  define('DB_USERNAME', 'usuario');
+  define('DB_PASSWORD', 'senha');
+  define('DB_DATABASE', 'bancodedados');
+  define('DB_PREFIX','prefixo');
+  define('DB_CHARSET','utf8');
+?>
