aa2d474e4f7379493db5334364c7ceab33314075
Ariane Brandão <ariane_mad@hotmail.com>
2018-11-03 00:40:19 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -25,7 +25,7 @@
     @mysqli_close($link) or die(mysqli_error($link));
   }
 
-  // Abre com Conexão com MySQL
+  // Conexão com MySQLi
   function DBConnect(){
     $link = @mysqli_connect(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die(mysqli_connect_error());
     mysqli_set_charset($link, DB_CHARSET) or die(mysqli_error($link));
